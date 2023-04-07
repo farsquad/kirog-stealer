facacced067c88af3d7d58464016baa414b8f4ad
farsquad <126510877+farsquad@users.noreply.github.com>
2023-04-07 17:45:21 -0400

    Add files via upload

---
+++ Stealer Priv/build.bat
@@ -0,0 +1,30 @@
+@echo off
+mode con: cols=100 lines=30
+title Stealer
+color 0a
+cls
+
+python --version 2>&1 | findstr " 3.11" >nul
+if %errorlevel% == 0 (
+    echo python 3.11.x No Soporta, instalate python 3.10.x.
+    pause
+    exit
+)
+
+::
+git --version 2>&1>nul
+if %errorlevel% == 9009 (
+    echo instalate git https://git-scm.com/download/win
+    pause
+    exit
+)
+
+py -3.10 -m pip uninstall -r interferences.txt
+py -3.10 -m pip install --upgrade -r requirements.txt
+
+cls
+
+if exist build rmdir /s /q build
+py -3.10 builder.py
+
+pause
